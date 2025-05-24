724b7f4dda8c70abac318a7962d97aca197d0e1e
amine21345 <medaminemehrez123@gmail.com>
2025-05-24 13:36:08 +0100

    last udpates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import avatar from "../assets/avatars/Image-1.png";
 const Navbar = () => {
   return (
     <div className="w-[90%] my-4 bg-white rounded-lg px-4 py-2 flex justify-end items-center shadow-md gap-3">
@@ -6,7 +7,7 @@ const Navbar = () => {
         <p className="text-xs text-gray-500">Profile</p>
       </div>
       <img
-        src="/path/to/avatar.jpg" // Replace with actual image path
+        src={avatar}
         alt="John Doe"
         className="w-8 h-8 rounded-full object-cover"
       />
